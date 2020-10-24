5540c4e84ef3b9bf872605971c60812ab420a17a
rafacoelho4 <rafaelmalto@hotmail.com>
2020-10-24 19:00:45 -0300

    Deploy servidor com postgres

--- ormconfig.js
+++ ormconfig.js
@@ -1,7 +1,7 @@
 module.exports = {
   type: 'postgres',
   host: process.env.TYPEORM_HOST,
-  port: 5433,
+  port: process.env.TYPEORM_PORT,
   username: process.env.TYPEORM_USERNAME,
   password: process.env.TYPEORM_PASSWORD,
   database: process.env.TYPEORM_DATABASE,
